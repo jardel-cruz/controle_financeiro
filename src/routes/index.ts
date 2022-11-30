c939c239a0b16e2de0fea6439e811aa0968914f0
jardel <jardelcruz333@gmail.com>
2022-11-30 18:56:25 -0300

    Adicionando as rotas de cadastro de usuarios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import type { Application } from "express";
 import { receitasRouter } from "./receitasRoutes.js";
 import { despesasRouter } from "./despesasRoutes.js";
 import { resumoRouter } from "./resumoRoute.js";
+import { userRouter } from "./userRoutes.js";
 
 export const routes = (app: Application) => {
   app.use(receitasRouter);
   app.use(despesasRouter);
   app.use(resumoRouter);
+  app.use(userRouter);
 };
